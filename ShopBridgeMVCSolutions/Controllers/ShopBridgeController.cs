e706d3bc72db44c69008bc47c951585cb099c19a
Abhi <akhilaabhi14@gmail.com>
2021-11-11 14:36:02 +0300

    my project

---
+++ ShopBridgeMVCSolutions/Controllers/ShopBridgeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;////using System.Web;
+using System.Web.Mvc;
+using ShopBridgeEntities;
+using ShopBridgeServiceProvider;
+using System.Threading.Tasks;
+
+namespace ShopBridgeMVCSolutions.Controllers
+{
+    public class ShopBridgeController : Controller
+    {
+        // GET: ShopBridge
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ItemMaster()
+        {
+            ItemModel objItem = new ItemModel();
+            if (Request.QueryString["id"] != null)
+            {
+                objItem = GetOneItem(Convert.ToInt32(Request.QueryString["id"]));
+            }
+            string a = Request.QueryString["id"];
+                        
+            return View(objItem);
+        }
+        
+        public ActionResult SearchItem()
+        {
+            return View();
+        }
+
+        private ItemModel GetOneItem(int ItemId)
+        {
+            ItemsListModel objResponse = new ItemsListModel();
+            ItemModel objModel = new ItemModel();
+            objModel.ItemId = ItemId;
+            objResponse = ShopBridgeProvider.GetItems(objModel);
+            if (objResponse.IsValid)
+            {
+                foreach (ItemModel objItemModel in objResponse.ItemsList)
+                {
+                    objModel = objItemModel;
+                }
+            }
+            return objModel;
+        }
+        public JsonResult GetSearchItems()
+        {
+            ItemsListModel objResponse = new ItemsListModel();
+            SearchRequestModel objRequest = new SearchRequestModel();
+            objRequest.CategoryId = 6;
+            objRequest.UnitId = 6;
+            objRequest.SearchText = "";
+            objRequest.SortOrder = "itemModifiedDate Desc";
+            objResponse = ShopBridgeProvider.SearchItems(objRequest);
+            if (objResponse.IsValid)
+                return Json(objResponse.ItemsList, JsonRequestBehavior.AllowGet);
+            else
+                return Json(new List<ItemModel>(), JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SaveItem(ItemModel objModel)
+        {
+            ShopBridgeResponseModel objResponseModel = new ShopBridgeResponseModel();
+            objResponseModel = ShopBridgeProvider.SaveItem(objModel);
+            
+            return Json(new { success = objResponseModel.IsValid, message = objResponseModel.ResponseMessage }, JsonRequestBehavior.DenyGet);
+           
+        }
+
+        [HttpPost]
+        public ActionResult DeleteItem(int itemId)
+        {
+            ShopBridgeResponseModel objResponse = new ShopBridgeResponseModel();
+
+            ItemModel objModel = new ItemModel();
+            objModel.ItemId = Convert.ToInt32(itemId);
+            objResponse = ShopBridgeProvider.DeleteItem(objModel);
+            return Json(new { success = objResponse.IsValid, message = objResponse.ResponseMessage }, JsonRequestBehavior.DenyGet);
+            
+
+        }
+
+    }
+}
---
+++ ShopBridgeMVCSolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopBridgeMVCSolutions.Startup))]
+namespace ShopBridgeMVCSolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
